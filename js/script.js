e3f82a9940bc8b660b60a4cdcd998430d948bdfa
afruzbekh <afruzbek1595@gmail.com>
2024-06-21 09:38:12 +0500

    Inital commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    text.style.marginTop = value * 1.9 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill1.style.top = value * 0.8 + 'px';
+
+});
+
+
+const navbar = document.querySelector('.header');
+window.onscroll = () => {
+    if (window.scrollY > 170) {
+        navbar.classList.add('white');
+    } else {
+        navbar.classList.remove('white');
+    }
+};
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
